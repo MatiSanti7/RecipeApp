787d3d97d598ea7cab86d19575b5d043ea3277d6
Matias <matiassantillan052@gmail.com>
2024-05-27 12:52:30 -0300

    Fix Footer Section

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,42 +5,40 @@ const Footer = () => {
         <img src="./src/assets/img/logo.png" alt="" className="mx-auto w-28" />
         <div className="relative flex items-center justify-between">
           <ul className="absolute flex mx-auto transform -translate-x-1/2 gap-14 left-1/2">
-            <li className="text-xl">
+            <li className="text-xl hover:text-white">
               <a href="/home">Home</a>
             </li>
-            <li className="text-xl">
+            <li className="text-xl hover:text-white">
               <a href="/add-menu">Add Menu</a>
             </li>
-            <li className="text-xl">
+            <li className="text-xl hover:text-white">
               <a href="/search-menu">Search Menu</a>
             </li>
-            <li className="text-xl">
+            <li className="text-xl hover:text-white">
               <a href="/profile">Your Recipes</a>
             </li>
           </ul>
           <ul className="flex gap-3 ml-auto">
-            <li>
-              <a href="">
-                <i
-                  className="text-2xl fa fa-facebook-official"
-                  aria-hidden="true"
-                ></i>
+            <li className="text-2xl hover:text-white">
+              <a href="https://www.facebook.com" target="_blank">
+                <i className="fa fa-facebook-official" aria-hidden="true"></i>
               </a>
             </li>
-            <li>
-              <a href="">
-                <i className="text-2xl fa fa-instagram" aria-hidden="true"></i>
+            <li className="text-2xl hover:text-white">
+              <a href="https://www.instagram.com" target="_blank">
+                <i className="fa fa-instagram" aria-hidden="true"></i>
               </a>
             </li>
-            <li>
-              <a href="">
-                <i className="text-2xl fa fa-youtube" aria-hidden="true"></i>
+            <li className="text-2xl hover:text-white">
+              <a href="https://www.youtube.com" target="_blank">
+                <i className="fa fa-youtube" aria-hidden="true"></i>
               </a>
             </li>
-            <a href="">
-              <i className="text-2xl fa fa-pinterest" aria-hidden="true"></i>
-            </a>
-            <li></li>
+            <li className="text-2xl hover:text-white">
+              <a href="https://www.pinterest.com" target="_blank">
+                <i className="fa fa-pinterest" aria-hidden="true"></i>
+              </a>
+            </li>
           </ul>
         </div>
       </div>
