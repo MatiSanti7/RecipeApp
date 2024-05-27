d2f60d4c5cec2fa35b7f2763d4f44598577ebb66
Matias <matiassantillan052@gmail.com>
2024-05-27 19:40:25 -0300

    Added VerifyUser Login SignUp

--- src/components/LogIn.jsx
+++ src/components/LogIn.jsx
@@ -1,4 +1,24 @@
+import users from "../assets/data/users.json";
+import { useNavigate } from "react-router-dom";
+
 const LogIn = () => {
+  const navigate = useNavigate();
+
+  const verificatedLogin = (e) => {
+    e.preventDefault();
+    const emailValue = e.target.email.value;
+    const passwordValue = e.target.password.value;
+    const tAC = e.target.tAC.checked;
+    const verifyUser = users.find(
+      (user) => user.Email === emailValue && user.Password === passwordValue
+    );
+    if (verifyUser && tAC) {
+      navigate("/home");
+    } else {
+      alert("Email or Password is incorrect");
+    }
+  };
+
   return (
     <div className="flex flex-col justify-center h-screen mx-auto max-w-[400px]">
       <h2 className="text-[#EFC81A] text-3xl mb-5 text-center">Recipe..</h2>
@@ -9,39 +29,41 @@ const LogIn = () => {
         Log into your exisiting account
       </small>
       <hr className="h-[2px] bg-gray-200 my-6 w-11/12 mx-auto" />
-      <form action="" className="flex flex-col gap-3">
+      <form
+        action=""
+        className="flex flex-col gap-3"
+        onSubmit={verificatedLogin}
+      >
         <div className="flex flex-col gap-1">
-          <label className="p-0 text-xl font-light" htmlFor="">
+          <label className="p-0 text-xl font-light" htmlFor="email">
             Email
           </label>
           <input
             type="email"
-            name=""
-            id=""
+            id="email"
             placeholder="Your Email Here"
             className="px-3 py-2 border-2 border-gray-200 rounded-md focus:outline-none placeholder:font-light"
           />
         </div>
         <div className="flex flex-col gap-1">
-          <label className="p-0 text-xl font-light" htmlFor="">
+          <label className="p-0 text-xl font-light" htmlFor="password">
             Password
           </label>
           <input
             type="password"
-            name=""
-            id=""
+            id="password"
             placeholder="Your Password Here"
             className="px-3 py-2 border-2 border-gray-200 rounded-md focus:outline-none placeholder:font-light"
           />
         </div>
         <div className="flex items-center gap-4">
-          <input type="checkbox" name="" id="" className="w-4 h-4" />
-          <label htmlFor="">I agree to teams & conditions</label>
+          <input type="checkbox" id="tAC" name="tAC" className="w-4 h-4" />
+          <label htmlFor="tAC">I agree to teams & conditions</label>
         </div>
         <input
           type="submit"
           value="Login"
-          className="bg-[#EFC81A] text-white py-2 text-xl rounded-md"
+          className="bg-[#EFC81A] text-white py-2 text-xl rounded-md cursor-pointer"
         />
       </form>
       <small className="mt-4 text-base text-center">
--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -1,59 +1,77 @@
+import { useNavigate } from "react-router-dom";
+
 const SignUp = () => {
+  const navigate = useNavigate();
+
+  const handleSignUp = (e) => {
+    e.preventDefault();
+    const checkbox = e.target.querySelector("input[type='checkbox']").checked;
+    const name = e.target.querySelector("input[name='name']").value;
+    const email = e.target.querySelector("input[name='email']").value;
+    const password = e.target.querySelector("input[name='password']").value;
+
+    if (checkbox && name && email && password) {
+      navigate("/home");
+    } else {
+      alert("Please fill all the fields and agree to the terms and conditions");
+    }
+  };
+
   return (
     <div className="flex flex-col justify-center h-screen mx-auto max-w-[400px]">
       <h2 className="text-[#EFC81A] text-3xl mb-5 text-center">Recipe..</h2>
       <h1 className="text-[#EFC81A] text-4xl text-center">
-        Let´s Get you set up !
+        Let´s Get you set up!
       </h1>
       <small className="text-[#8692A6] text-center text-lg">
         Create a new account to access all features
       </small>
       <hr className="h-[2px] bg-gray-200 my-6 w-11/12 mx-auto" />
-      <form action="" className="flex flex-col gap-4">
+      <form className="flex flex-col gap-4" onSubmit={handleSignUp}>
         <div className="flex flex-col gap-1">
-          <label className="p-0 text-xl font-light" htmlFor="">
+          <label className="p-0 text-xl font-light" htmlFor="name">
             Name
           </label>
           <input
             type="text"
-            name=""
-            id=""
+            name="name"
+            id="name"
             placeholder="Your Name Here"
             className="px-3 py-2 border-2 border-gray-200 rounded-md focus:outline-none placeholder:font-light"
           />
         </div>
         <div className="flex flex-col gap-1">
-          <label className="p-0 text-xl font-light" htmlFor="">
+          <label className="p-0 text-xl font-light" htmlFor="email">
             Email
           </label>
           <input
             type="email"
-            name=""
-            id=""
+            name="email"
+            id="email"
             placeholder="Your Email Here"
             className="px-3 py-2 border-2 border-gray-200 rounded-md focus:outline-none placeholder:font-light"
           />
         </div>
         <div className="flex flex-col gap-1">
-          <label className="p-0 text-xl font-light" htmlFor="">
+          <label className="p-0 text-xl font-light" htmlFor="password">
             Password
           </label>
           <input
             type="password"
-            name=""
-            id=""
+            name="password"
+            id="password"
             placeholder="Your Password Here"
             className="px-3 py-2 border-2 border-gray-200 rounded-md focus:outline-none placeholder:font-light"
           />
         </div>
         <div className="flex items-center gap-4">
-          <input type="checkbox" name="" id="" className="w-4 h-4" />
-          <label htmlFor="">I agree to teams & conditions</label>
+          <input type="checkbox" id="terms" className="w-4 h-4" />
+          <label htmlFor="terms">I agree to terms & conditions</label>
         </div>
         <input
           type="submit"
           value="Register Account"
-          className="bg-[#EFC81A] text-white py-2 text-xl rounded-md"
+          className="bg-[#EFC81A] text-white py-2 text-xl rounded-md cursor-pointer"
         />
       </form>
       <small className="mt-12 text-base text-center">
