433493a4547c4b8acaad223a71eb3509e60e69fc
Matias <matiassantillan052@gmail.com>
2024-05-27 19:41:20 -0300

    Added Show All Recipes

--- src/components/SearchMenu.jsx
+++ src/components/SearchMenu.jsx
@@ -1,5 +1,7 @@
 import Nav from "./Nav";
 import Footer from "./Footer";
+import recipesData from "../assets/data/recipes.json";
+import users from "../assets/data/users.json";
 
 const SearchMenu = () => {
   return (
@@ -23,38 +25,56 @@ const SearchMenu = () => {
         </div>
         <hr className="h-1 w-full bg-[#EFC81A] my-12" />
         <div className="flex flex-col gap-8">
-          <div className="flex gap-16">
-            <img
-              src="./src/assets/img/image-1.png"
-              alt=""
-              className="object-cover w-1/5 rounded-md"
-            />
-            <div className="flex flex-col justify-between 4/5">
-              <div className="flex flex-col gap-3">
-                <h2 className="text-3xl font-medium">
-                  Recipe Title 1 Made By {"<USER NAME>"}
-                </h2>
-                <strong className="text-xl font-normal">Ingredients</strong>
-                <p className="text-lg">Description Here</p>
-              </div>
-              <div className="flex gap-10">
-                <div className="flex items-center gap-2 text-xl">
-                  <i
-                    className="cursor-pointer fa fa-heart-o hover:text-red-600"
-                    aria-hidden="true"
-                  ></i>
-                  <small>Like Recipe</small>
+          {recipesData.recipes.map((recipe, key) => {
+            return (
+              <div key={key} className="flex h-56 gap-16">
+                <img
+                  src={recipe.image}
+                  alt={recipe.title}
+                  className="object-cover w-1/5 rounded-md"
+                />
+                <div className="flex flex-col justify-between 4/5">
+                  <div className="flex flex-col gap-3">
+                    <h2 className="mt-2 text-3xl font-medium">
+                      {recipe.title} Made By
+                      <span className="text-[#EFC81A]">
+                        {" " + users[recipe.author].Name}
+                      </span>
+                    </h2>
+                    <ul className="flex flex-wrap gap-2">
+                      {recipe.ingredients.map((i, index) => {
+                        return (
+                          <li
+                            key={index}
+                            className="text-xl font-normal bg-[#efc81a67] py-1 px-3 rounded-2xl"
+                          >
+                            {i}
+                          </li>
+                        );
+                      })}
+                    </ul>
+                    <p className="text-lg">{recipe.description}</p>
+                  </div>
+                  <div className="flex gap-10">
+                    <div className="flex items-center gap-2 text-xl">
+                      <i
+                        className="cursor-pointer fa fa-heart-o hover:text-red-600"
+                        aria-hidden="true"
+                      ></i>
+                      <small>Like Recipe</small>
+                    </div>
+                    <div className="flex items-center gap-2 text-xl">
+                      <i
+                        className="fa fa-bookmark-o hover:text-[#EFC81A] cursor-pointer"
+                        aria-hidden="true"
+                      ></i>
+                      <small>Bookmark Recipe</small>
+                    </div>
+                  </div>
                 </div>
-                <div className="flex items-center gap-2 text-xl">
-                  <i
-                    className="fa fa-bookmark-o hover:text-[#EFC81A] cursor-pointer"
-                    aria-hidden="true"
-                  ></i>
-                  <small>Bookmark Recipe</small>
-                </div>
               </div>
-            </div>
-          </div>
+            );
+          })}
         </div>
       </section>
       <Footer />
