f77cb41741453231b80312431d8c717093963347
Matias <matiassantillan052@gmail.com>
2024-05-28 19:57:52 -0300

    Fixed Link and Image

--- src/components/Home/Category.jsx
+++ src/components/Home/Category.jsx
@@ -3,9 +3,12 @@ import Footer from "../Footer";
 import PropTypes from "prop-types";
 import recipesData from "../../assets/data/recipes.json";
 import users from "../../assets/data/users.json";
-import { useLocation } from "react-router-dom";
+import { useLocation, Link } from "react-router-dom";
+import { useRef } from "react";
 
 const Category = ({ category }) => {
+  const sectionRef = useRef(null);
+
   let recipes = [];
   let isCategoryRegion =
     category == "europe" ||
@@ -60,20 +63,31 @@ const Category = ({ category }) => {
         <div className="flex flex-col gap-8">
           {recipes.map((recipe, key) => {
             return (
-              <div className="flex h-56 gap-16" key={key}>
-                <img
-                  src={recipe.image}
-                  alt={recipe.title}
-                  className="object-cover w-1/5 rounded-md"
-                />
-                <div className="flex flex-col justify-between 4/5">
+              <div className="flex gap-16" key={key}>
+                <div className="w-1/5 h-56">
+                  <img
+                    src={recipe.image}
+                    alt={recipe.title}
+                    className="object-cover w-full h-full rounded-md"
+                  />
+                </div>
+                <div className="flex flex-col justify-between w-4/5">
                   <div className="flex flex-col gap-3">
-                    <h2 className="mt-2 text-3xl font-medium">
-                      {recipe.title} Made By
-                      <span className="text-[#EFC81A]">
-                        {" " + users[recipe.author].Name}
-                      </span>
-                    </h2>
+                    <Link
+                      to={`/recipe/${recipe.id}`}
+                      onClick={() => {
+                        sectionRef.current.scrollIntoView({
+                          behavior: "smooth",
+                        });
+                      }}
+                    >
+                      <h2 className="mt-2 text-3xl font-medium hover:underline hover:underline-offset-8">
+                        {recipe.title} Made By
+                        <span className="text-[#EFC81A]">
+                          {" " + users[recipe.author].Name}
+                        </span>
+                      </h2>
+                    </Link>
                     <ul className="flex flex-wrap gap-2">
                       {recipe.ingredients.map((i, index) => {
                         return (
@@ -91,14 +105,19 @@ const Category = ({ category }) => {
                   <div className="flex gap-10">
                     <div className="flex items-center gap-2 text-xl">
                       <i
-                        className="cursor-pointer fa fa-heart-o hover:text-red-600"
-                        aria-hidden="true"
+                        className={`cursor-pointer fa-heart ${
+                          recipe.liked ? "fa-solid text-red-600" : "fa-regular"
+                        }`}
                       ></i>
                       <small>Like Recipe</small>
                     </div>
                     <div className="flex items-center gap-2 text-xl">
                       <i
-                        className="fa fa-bookmark-o hover:text-[#EFC81A] cursor-pointer"
+                        className={`fa fa-bookmark ${
+                          recipe.bookmarked
+                            ? "fa-solid text-[#EFC81A]"
+                            : "fa-regular"
+                        } cursor-pointer`}
                         aria-hidden="true"
                       ></i>
                       <small>Bookmark Recipe</small>
